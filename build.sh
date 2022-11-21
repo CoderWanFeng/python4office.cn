23f80d9bb1e3effba986fc4b4b797e9743ce0252
程序员晚枫 <1529577833@qq.com>
2022-11-22 06:52:04 +0800

    use nginx

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd /opt/workplace/pro/python4office.cn/hexo/hexo
+yarn install
+hexo clean
+hexo g
--- hexo/hexo/start.sh
+++
@@ -1,6 +0,0 @@
-cd /opt/workplace/pro/python4office.cn/hexo/hexo
-npm install
-hexo clean
-hexo g
-# hexo s 
-hexo s -p 18001
