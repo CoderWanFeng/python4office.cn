90c04e2bf71db7e6c0f91a6d9d065c9ede145410
CoderWanFeng <875143930@qq.com>
2023-08-20 00:25:16 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ yarn install
 yarn run clean
 yarn run build
 
-mkdir /home/ubuntu/workplace/pro/website/python4office.cn
 rm -rf /home/ubuntu/workplace/pro/website/python4office.cn/*
 cp /home/ubuntu/workplace/pro/python4office.cn/hexo/hexo/public/* /home/ubuntu/workplace/pro/website/python4office.cn/ -R
