31d9f906bf35a5b44b4accabd73f9901d25091d9
CoderWanFeng <875143930@qq.com>
2023-08-20 14:07:20 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-cd /opt/workplace/pro/python4office.cn/hexo/hexo
+cd hexo/hexo
 yarn install
-hexo clean
-hexo g
+yarn run clean
+yarn run build
 
 rm -rf /opt/website/python4office.cn/*
-cp /opt/workplace/pro/python4office.cn/hexo/hexo/public/* /opt/website/python4office.cn/ -R 
+cp /opt/workplace/pro/python4office.cn/hexo/hexo/public/* /opt/website/python4office.cn/ -R
