c0df86fc1f3cb5b44df852c849d9aea20622aae0
CoderWanFeng <1957875073@qq.com>
2024-12-09 21:58:17 +0800

    保留build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,5 @@ yarn install
 yarn run clean
 yarn run build
 
-git add .
-git commit -m "update"
-git push
-#rm -rf /opt/website/python4office.cn/*
-#cp /opt/workplace/pro/python4office.cn/hexo/hexo/public/* /opt/website/python4office.cn/ -R
+rm -rf /opt/website/python4office.cn/*
+cp /opt/workplace/pro/python4office.cn/hexo/hexo/public/* /opt/website/python4office.cn/ -R
