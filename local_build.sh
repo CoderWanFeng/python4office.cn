59be77ca81cb0609c982d99e4b828f20769e0133
CoderWanFeng <1957875073@qq.com>
2024-12-16 23:41:37 +0800

    更新网站

--- local_build.sh
+++ local_build.sh
@@ -2,9 +2,10 @@ cd hexo/hexo
 yarn install
 yarn run clean
 yarn run build
+yarn run server
 
-git add .
-git commit -m "更新网站"
-git push
+# git add .
+# git commit -m "更新网站"
+# git push
 #rm -rf /opt/website/python4office.cn/*
 #cp /opt/workplace/pro/python4office.cn/hexo/hexo/public/* /opt/website/python4office.cn/ -R
